02bf33aabea17d1b2793a3655d77aba5d7d8c27a
Aamin Raja <aaminraja21@gmail.com.com>
2024-10-27 10:13:28 +0530

    backend hosting

--- logger.js
+++ logger.js
@@ -3,18 +3,18 @@ const winston = require('winston');
 const path = require('path');
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ 
-      filename: path.join(__dirname, 'logs', 'application.log'),
-      level: 'info'
-    })
-  ]
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: path.join(__dirname, 'logs', 'application.log'),
+            level: 'info'
+        })
+    ]
 });
 
 module.exports = logger;
